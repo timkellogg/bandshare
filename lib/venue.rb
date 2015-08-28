9f516c80fc4958d6d270cb5a2bf1aee3692ece59
timkellogg <tim.kellogg@gmail.com>
2015-08-28 09:40:43 -0700

    add validations, callbacks and testing for venue

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,10 +1,11 @@
 class Venue < ActiveRecord::Base 
 	has_and_belongs_to_many :bands, join_table: 'concerts'
+	validates :name, presence: true 
+	before_save :capitalize_name
+
+	private 
+
+		def capitalize_name
+			self.name = self.name.split.collect(&:capitalize).join(' ')
+		end
 end
-
-
-	# Implement a validation
-	# Add a callback
-
-	# As a user, I do not want bands and venues to be saved if I enter a blank name.
-	# 	As a user, I want band names and venues to be saved with a capital letter no matter how I enter them.
--- spec/venue_spec.rb
+++ spec/venue_spec.rb
@@ -1,4 +1,22 @@
 require 'spec_helper'
 
 describe Venue do  
+
+	it { should have_and_belong_to_many :bands  }
+	it { should validate_presence_of    :name   }
+
+	context 'when saving the venue into the database' do  
+
+		it 'should capitalize multiple word venue names' do  
+			@venue = Venue.create(name: "hollywood bowl", city: "Los Angeles", capacity: 17376, 
+				    pic_link: "http://www.seeing-stars.com/Images/Slides/Bowl.JPG", country: "America")
+			expect(@venue.name).to eq 'Hollywood Bowl'
+		end
+
+		it 'should capitalize singular word venue names' do  
+			@venue = Venue.create(name: "holocene", city: "Portland", capacity: 326, 
+				    pic_link: "http://www.holocene.org/photos/wedding_toast_large.jpg", country: "America")			
+			expect(@venue.name).to eq 'Holocene'
+		end
+	end
 end

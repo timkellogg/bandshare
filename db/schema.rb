6807f1a69643072f5b0633f32ace8178b7db3366
timkellogg <tim.kellogg@gmail.com>
2015-08-28 13:10:10 -0700

    add concert form date of concert

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150828182634) do
+ActiveRecord::Schema.define(version: 20150828200920) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20150828182634) do
     t.boolean  "played_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "date"
   end
 
   add_index "concerts", ["band_id"], name: "index_concerts_on_band_id", using: :btree

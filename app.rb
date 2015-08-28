acb4e5d6415e7a86feffe118227a59114fb6d4fb
timkellogg <tim.kellogg@gmail.com>
2015-08-28 00:43:36 -0700

    sinatra boilerplate with basic routes, spec setup and empty views

---
+++ app.rb
@@ -0,0 +1,140 @@
+require 'bundler/setup'
+Bundler.require :default, :test
+
+Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+after { ActiveRecord::Base.connection.close }
+
+# Bands
+
+# Landing page
+get '/' do 
+	erb :index
+end
+
+# Index (bands)
+get '/bands' do 
+	erb :bands 
+end
+
+# Show (bands)
+get '/bands/:id' do  
+	@band = Bands.find(params['id'])
+	erb :band
+end
+
+# New (bands)
+get '/bands/new' do  
+	erb :create_band_form 
+end
+
+# Create (bands)
+post '/bands' do 
+	# get params 
+
+	# create object 
+
+	if @band.save 
+		erb :band 
+	else 
+		redirect '/bands/new'
+	end
+end
+
+# Edit (bands)
+get '/bands/:id/edit' do  
+	@band = Band.find(params['id'])
+	erb :edit_band_form 
+end
+
+
+# Update (bands)
+patch '/bands/:id' do  
+	@band = Band.find(params['id'])
+
+	# Fetch params 
+
+	if @band.update({})
+		erb: band 
+	else 
+		redirect "/bands/#{band.id}/edit"
+	end
+end
+
+# Delete (bands)
+get '/bands/:id/delete' do  
+	band = Band.find(params['id'])
+
+	if band.delete 
+		erb :bands 
+	else 
+		redirect "/bands/#{band.id}" 
+	end
+end
+
+# Venues 
+
+# Index (venues)
+get '/venues' do  
+	@venues = Venue.all  
+	erb :venues 
+end
+
+# Show (venues)
+get '/venues/:id' do  
+	@venue = Venue.find(params['id'])
+	erb :venue 
+end
+
+# New (venues)
+get '/venues/new' do  
+	erb :create_venue_form 
+end
+
+# Create (venues)
+post '/venues' do  
+	# fetch params 
+
+	# create venue 
+
+	if @venue.save 
+		erb :venues 
+	else 
+		redirect "/venues/new"
+	end
+end
+
+# Edit (venues)
+get '/venues/:id/edit' do  
+	erb :edit_venue_form 
+end
+
+# Update (venues)
+patch '/bands/:id' do  
+	@venue = Venue.find(params['id'])
+
+	# Fetch params 
+
+	if @venue.update({})
+		erb: venue 
+	else 
+		redirect "/venues/#{venue.id}/edit"
+	end
+end
+
+# Delete (venues)
+get '/venues/:id/delete' do  
+	venue = Venue.find(params['id'])
+
+	if venue.delete 
+		erb :venues 
+	else 
+		redirect "/venues/#{venue.id}" 
+	end
+end
+
+
+
+
+
+
---
+++ lib/band.rb
@@ -0,0 +1,8 @@
+class Band < ActiveRecord::Base 
+
+	Implement a validation
+	Add a callback
+
+	As a user, I do not want bands and venues to be saved if I enter a blank name.
+		As a user, I want band names and venues to be saved with a capital letter no matter how I enter them.
+end

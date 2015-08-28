d5e4c46a43eb1e442f3caf763b7920b96c585cc6
timkellogg <tim.kellogg@gmail.com>
2015-08-28 09:10:00 -0700

    test relationships between venues and bands with custom-name join table "concerts"

--- app.rb
+++ app.rb
@@ -8,130 +8,130 @@ after { ActiveRecord::Base.connection.close }
 # Bands
 
 # Landing page
-get '/' do 
-	erb :index
-end
+# get '/' do 
+# 	erb :index
+# end
 
-# Index (bands)
-get '/bands' do 
-	erb :bands 
-end
+# # Index (bands)
+# get '/bands' do 
+# 	erb :bands 
+# end
 
-# Show (bands)
-get '/bands/:id' do  
-	@band = Bands.find(params['id'])
-	erb :band
-end
+# # Show (bands)
+# get '/bands/:id' do  
+# 	@band = Bands.find(params['id'])
+# 	erb :band
+# end
 
-# New (bands)
-get '/bands/new' do  
-	erb :create_band_form 
-end
+# # New (bands)
+# get '/bands/new' do  
+# 	erb :create_band_form 
+# end
 
-# Create (bands)
-post '/bands' do 
-	# get params 
+# # Create (bands)
+# post '/bands' do 
+# 	# get params 
 
-	# create object 
+# 	# create object 
 
-	if @band.save 
-		erb :band 
-	else 
-		redirect '/bands/new'
-	end
-end
+# 	if @band.save 
+# 		erb :band 
+# 	else 
+# 		redirect '/bands/new'
+# 	end
+# end
 
-# Edit (bands)
-get '/bands/:id/edit' do  
-	@band = Band.find(params['id'])
-	erb :edit_band_form 
-end
+# # Edit (bands)
+# get '/bands/:id/edit' do  
+# 	@band = Band.find(params['id'])
+# 	erb :edit_band_form 
+# end
 
 
-# Update (bands)
-patch '/bands/:id' do  
-	@band = Band.find(params['id'])
+# # Update (bands)
+# patch '/bands/:id' do  
+# 	@band = Band.find(params['id'])
 
-	# Fetch params 
+# 	# Fetch params 
 
-	if @band.update({})
-		erb: band 
-	else 
-		redirect "/bands/#{band.id}/edit"
-	end
-end
+# 	if @band.update({})
+# 		erb: band 
+# 	else 
+# 		redirect "/bands/#{band.id}/edit"
+# 	end
+# end
 
-# Delete (bands)
-get '/bands/:id/delete' do  
-	band = Band.find(params['id'])
+# # Delete (bands)
+# get '/bands/:id/delete' do  
+# 	band = Band.find(params['id'])
 
-	if band.delete 
-		erb :bands 
-	else 
-		redirect "/bands/#{band.id}" 
-	end
-end
+# 	if band.delete 
+# 		erb :bands 
+# 	else 
+# 		redirect "/bands/#{band.id}" 
+# 	end
+# end
 
-# Venues 
+# # Venues 
 
-# Index (venues)
-get '/venues' do  
-	@venues = Venue.all  
-	erb :venues 
-end
+# # Index (venues)
+# get '/venues' do  
+# 	@venues = Venue.all  
+# 	erb :venues 
+# end
 
-# Show (venues)
-get '/venues/:id' do  
-	@venue = Venue.find(params['id'])
-	erb :venue 
-end
+# # Show (venues)
+# get '/venues/:id' do  
+# 	@venue = Venue.find(params['id'])
+# 	erb :venue 
+# end
 
-# New (venues)
-get '/venues/new' do  
-	erb :create_venue_form 
-end
+# # New (venues)
+# get '/venues/new' do  
+# 	erb :create_venue_form 
+# end
 
-# Create (venues)
-post '/venues' do  
-	# fetch params 
+# # Create (venues)
+# post '/venues' do  
+# 	# fetch params 
 
-	# create venue 
+# 	# create venue 
 
-	if @venue.save 
-		erb :venues 
-	else 
-		redirect "/venues/new"
-	end
-end
+# 	if @venue.save 
+# 		erb :venues 
+# 	else 
+# 		redirect "/venues/new"
+# 	end
+# end
 
-# Edit (venues)
-get '/venues/:id/edit' do  
-	erb :edit_venue_form 
-end
+# # Edit (venues)
+# get '/venues/:id/edit' do  
+# 	erb :edit_venue_form 
+# end
 
-# Update (venues)
-patch '/bands/:id' do  
-	@venue = Venue.find(params['id'])
+# # Update (venues)
+# patch '/bands/:id' do  
+# 	@venue = Venue.find(params['id'])
 
-	# Fetch params 
+# 	# Fetch params 
 
-	if @venue.update({})
-		erb: venue 
-	else 
-		redirect "/venues/#{venue.id}/edit"
-	end
-end
+# 	if @venue.update({})
+# 		erb: venue 
+# 	else 
+# 		redirect "/venues/#{venue.id}/edit"
+# 	end
+# end
 
-# Delete (venues)
-get '/venues/:id/delete' do  
-	venue = Venue.find(params['id'])
+# # Delete (venues)
+# get '/venues/:id/delete' do  
+# 	venue = Venue.find(params['id'])
 
-	if venue.delete 
-		erb :venues 
-	else 
-		redirect "/venues/#{venue.id}" 
-	end
-end
+# 	if venue.delete 
+# 		erb :venues 
+# 	else 
+# 		redirect "/venues/#{venue.id}" 
+# 	end
+# end
 
 
 
--- lib/band.rb
+++ lib/band.rb
@@ -1,8 +1,10 @@
 class Band < ActiveRecord::Base 
+	has_and_belongs_to_many :venues, join_table: 'concerts'
 
-	Implement a validation
-	Add a callback
 
-	As a user, I do not want bands and venues to be saved if I enter a blank name.
-		As a user, I want band names and venues to be saved with a capital letter no matter how I enter them.
+	# Implement a validation
+	# Add a callback
+
+	# As a user, I do not want bands and venues to be saved if I enter a blank name.
+	# 	As a user, I want band names and venues to be saved with a capital letter no matter how I enter them.
 end
